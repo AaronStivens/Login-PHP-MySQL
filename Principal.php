27ee9358541d23d9e80923ddae5b37d02ddd1ffe
Aaron Hernandez <AaronHernandez14963@gmail.com>
2023-11-26 20:00:40 -0600

    variable de sesion

--- Principal.php
+++ Principal.php
@@ -10,18 +10,15 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+<body>
 
-<body class="loggedin">
-    <nav class="navtop">
-        <h1 style="color:white;">Sistema de Login Básico ConfiguroWeb</h1>
-        <a href="perfil.php" style="color:white;"><i class="fas fa-user-circle"></i>Informción de Usuario</a>
-        <a href="cerrar-sesion.php" style="color:white;"><i class="fas fa-sign-out-alt"></i>Cerrar Sesión</a>
-    </nav>
-
-    <div class="content">
-        <h2>Página de Inicio</h2>
-        <p>Hola de nuevo, <?= $_SESSION['name'] ?> !!!</p>
-    </div>
+<?php
+      session_start();
+      if(isset($_SESSION["Usuario"])){
+        echo "<h1>Bienvenido: ". $_SESSION["Usuario"]."</h1>";
+      }
+      ?>
+    
 </body>
 
 </html>
